7754780a5451d0e589b62884e3fbe381a235c614
qiulinyang <943280632@qq.com>
2024-04-23 21:03:02 +0800

    中文编码问题

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const pythonProcess = spawn('python', ['mytest.py']);
 
 // 监听子进程的标准输出
 pythonProcess.stdout.on('data', (data) => {
+   // const output = data.toString('utf-8');
   console.log(`Python output:${data}`);
 });
 
 // 监听子进程的标准错误输出
 pythonProcess.stderr.on('data', (data) => {
-  console.error(`Python 错误：${data}`);
+  console.error(`Python error:${data}`);
 });
 
 // 向子进程发送数据
--- mytest.py
+++ mytest.py
@@ -1,15 +1,19 @@
 import sys
 
-# 从标准输入读取数据
-input_data = sys.stdin.read()
 
-# 在控制台输出接收到的数据
-print("Python 接收到数据:", input_data)
-
-# 对接收到的数据进行处理（示例：将接收到的数据转换为大写）
-input_data+=" I am added from python"
-output_data = input_data.upper()
+# input_data = sys.stdin.read()
 
 
-# 将处理结果发送回 Node.js
-sys.stdout.write(output_data)
+# print("Python receive data:", input_data)
+
+
+# input_data+=" I am added from python"
+# output_data = input_data.upper()
+# print("哈哈")
+import json
+sys.stdout.reconfigure(encoding='utf-8')
+
+dicts={"b":"哈哈","k":0}
+jsonStr = json.dumps(dicts, ensure_ascii=False)
+sys.stdout.write(jsonStr)
+
